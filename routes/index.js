1bbd577e49d5d6ef0b9173166d433c55e5d7fd87
tianhengzhou <zth198814@gmail.com>
2015-07-21 09:19:22 -0700

    Repository created

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+/*router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});*/
+
+/*module.exports = router;*/
+
+module.exports = function(app) {
+  app.get('/', function (req, res) {
+    res.render('index', { title: 'Home' });
+  });
+  app.get('/reg', function (req, res) {
+    res.render('index', { title: 'Register' });
+  });
+  app.get('/login', function (req, res) {
+    res.render('index', { title: 'Login' });
+  });
+  app.get('/post', function (req, res) {
+    res.render('index', { title: 'Post' });
+  });
+  app.get('/logout', function (req, res) {
+    res.render('index', { title: 'Home' });
+  });
+};
---
+++ settings.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Tim Zhou on 7/19/2015.
+ */
+module.exports = {
+    cookieSecret: 'myblog',
+    db: 'blog',
+    host: 'localhost',
+    port: 27017
+};
